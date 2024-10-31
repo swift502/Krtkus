7294327996078e0f4af8cd318361bae2ad387898
Jan Bláha <blaha.j502@gmail.com>
2024-10-31 20:14:47 +0100

    layout

--- source/krtkus_qmk/keymaps/default/keymap.c
+++ source/krtkus_qmk/keymaps/default/keymap.c
@@ -20,10 +20,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * └───┴───┴───┴───┴───┴───────┴───┴───┴───┴───┴───┘
      */
     [0] = LAYOUT_default(
-      KC_GRV,                                                                                            KC_DEL,
+      KC_ESC,                                                                                            KC_DEL,
       KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-      KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+      KC_GRV,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_ENT),
       KC_LCTL, KC_LGUI, KC_F13,  KC_LALT, MO(1),        KC_SPC,      MO(2),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
     ),
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
       _______, _______, KC_UP,   _______, KC_PGUP, KC_KP_7, KC_KP_8, KC_KP_9, _______, KC_LBRC, KC_RBRC, KC_BSLS,
       KC_CAPS, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, KC_KP_4, KC_KP_5, KC_KP_6, _______, _______, _______, _______,
-      _______, KC_BSLS, _______, _______, _______, KC_KP_1, KC_KP_2, KC_KP_3, _______, _______, _______, _______,
+      _______, KC_NUBS, _______, _______, _______, KC_KP_1, KC_KP_2, KC_KP_3, _______, _______, _______, _______,
       _______, _______, _______, _______, _______,      KC_KP_0,     _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
     ),
     [2] = LAYOUT_default(
