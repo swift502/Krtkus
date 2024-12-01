8b05fda9c7ea1b7d67068bab9357bdebddb9c0dc
Jan Bláha <blaha.j502@gmail.com>
2024-12-01 15:07:55 +0100

    keymap update, custom VIA keycode

--- source/qmk/keymaps/default/keymap.c
+++ source/qmk/keymaps/default/keymap.c
@@ -3,6 +3,25 @@
 
 #include QMK_KEYBOARD_H
 
+enum keyboard_keycodes {
+    KRT_VOL = QK_KB_0,
+};
+
+bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case KRT_VOL:
+            if (record->event.pressed) {
+                if (get_mods() & MOD_MASK_SHIFT) {
+                    tap_code(KC_VOLU);
+                } else {
+                    tap_code(KC_VOLD);
+                }
+            }
+            return false;
+    }
+    return process_record_user(keycode, record);
+}
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
     *  ┌───┐                                       ┌───┐
@@ -25,15 +44,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
       KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_ENT),
-      KC_LCTL, KC_LGUI, KC_F13,  KC_LALT, MO(1),        KC_SPC,      MO(2),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+      KC_LCTL, KC_LGUI, KRT_VOL,  KC_LALT, MO(1),        KC_SPC,      MO(2),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
     ),
     [1] = LAYOUT_default(
       _______,                                                                                           _______,
       KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-      _______, _______, KC_UP,   _______, _______, KC_KP_7, KC_KP_8, KC_KP_9, _______, KC_LBRC, KC_RBRC, KC_BSLS,
-      KC_CAPS, KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_KP_4, KC_KP_5, KC_KP_6, _______, _______, _______, _______,
-      _______, _______, _______, _______, _______, KC_KP_1, KC_KP_2, KC_KP_3, _______, _______, _______, _______,
-      _______, _______, _______, _______, _______,      KC_KP_0,     KC_PDOT, KC_HOME, KC_PGDN, KC_PGUP, KC_END
+      _______, _______, KC_UP,   _______, KC_PGUP, KC_KP_7, KC_KP_8, KC_KP_9, _______, _______, KC_LBRC, KC_RBRC,
+      KC_CAPS, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, KC_KP_4, KC_KP_5, KC_KP_6, _______, _______, _______, KC_BSLS,
+      _______, _______, _______, _______, _______, KC_KP_1, KC_KP_2, KC_KP_3, _______, _______, _______, KC_ENT,
+      _______, _______, _______, _______, _______,      KC_KP_0,     KC_PDOT, KC_HOME, _______, _______, KC_END
     ),
     [2] = LAYOUT_default(
       _______,                                                                                           _______,
