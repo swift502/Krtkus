986c210023eef572ee75a9b02d538f206c285645
Jan Bláha <blaha.j502@gmail.com>
2025-06-20 14:47:03 +0200

    shorthand codes

--- source/qmk/keymaps/default/keymap.c
+++ source/qmk/keymaps/default/keymap.c
@@ -27,10 +27,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [1] = LAYOUT_default(
         _______,                                                                                           _______,
         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-        _______, _______, KC_UP,   _______, _______, KC_KP_7, KC_KP_8, KC_KP_9, _______, _______, KC_LBRC, KC_RBRC,
-        KC_CAPS, KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_KP_4, KC_KP_5, KC_KP_6, _______, _______, _______, KC_BSLS,
-        _______, _______, _______, _______, _______, KC_KP_1, KC_KP_2, KC_KP_3, _______, _______, _______, KC_ENT,
-        _______, _______, _______, _______, _______,      KC_KP_0,     KC_PDOT, KC_HOME, KC_PGDN, KC_PGUP, KC_END
+        _______, _______, KC_UP,   _______, _______, KC_P7,   KC_P8,   KC_P9,   _______, _______, KC_LBRC, KC_RBRC,
+        KC_CAPS, KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_P4,   KC_P5,   KC_P6,   _______, _______, _______, KC_BSLS,
+        _______, _______, _______, _______, _______, KC_P1,   KC_P2,   KC_P3,   _______, _______, _______, KC_ENT,
+        _______, _______, _______, _______, _______,      KC_P0,       KC_PDOT, KC_HOME, KC_PGDN, KC_PGUP, KC_END
     ),
     [2] = LAYOUT_default(
         _______,                                                                                           _______,
